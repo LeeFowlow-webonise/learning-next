f9e5a829a1147cfc206f926121c258d85bb0e313
LeeFowlow-webonise <107060935+LeeFowlow-webonise@users.noreply.github.com>
2022-07-08 08:27:20 -0230

    faux article fetch and display set up

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,12 @@
+//Only renders on the server
+//Purpose is to allow access to the html and body tags
+//Can see that I've added 'lang="en"' to the HTML tag
+
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Head from 'next/head'//custom titles or meta tags
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6');
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles
+    }
+  }
+}
+
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
         <title>Learning Next</title>
         <meta name='keywords' keywords='web development, programming, Next.js'></meta>
       </Head>
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles}/>
     </div>
   )
 }
